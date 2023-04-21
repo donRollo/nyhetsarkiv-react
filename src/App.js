be52228c78c7e579a61b816199b74f7c4628da9b
Hydén Roland RK <rhn014@lthalland.se>
2023-04-21 15:19:01 +0200

    justeringar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
-import Footer from './Components/Footer';
 import Head from './Components/Head';
 import Header from './Components/Header';
 import Nyheter from './Components/Nyheter';
+import Footer from './Components/Footer';
+import Login from './Login';
 
 function App() {
   return (
