3f438917dd9305e86e208c6b3911cd622ff40d92
Hydén Roland RK <rhn014@lthalland.se>
2023-04-26 06:48:29 +0200

    Justerade från class till function

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,9 @@
-import React from 'react'
+import React from 'react';
+import useForceUpdate from 'use-force-update';
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 
-class Header extends React.Component {
+function Header() {
   
-  handleClick() {
-    this.forceUpdate();
-  }
-  
-  render() {
-
   var userSessionStorage = sessionStorage.getItem("userData");
   var hasUserData = false;
   if (userSessionStorage && userSessionStorage.length > 3) {
@@ -21,35 +16,41 @@ class Header extends React.Component {
     isLoginPage = true;
   }
 
-  return <div className="container mx-auto pt-8">  
-          <div className="pl-8 pr-12">
-            <div className="flex mb-4">
-              <div className="w-11/12">
-                <span className="center text-white text-xl">
-                  <h1>Välkommen till nyhetsarkivet</h1>
-                </span>
-              </div>
-              <div className="w-1/12">
-                {
-                  hasUserData ? 
-                    <span className="center text-white test-sm">
-                      <Link to="/Logout" onClick={this.handleClick.bind(this)}>[ LOGGA UT ]</Link>
-                    </span> 
-                  : isLoginPage ? 
-                    <span className="center text-white test-sm">
-                      <Link to="/" onClick={this.handleClick.bind(this)}>[ AVBRYT ]</Link>
-                    </span> 
-                  : 
-                    <span className="center text-white test-sm">
-                      <Link to="/Login" onClick={this.handleClick.bind(this)}>[ LOGGA IN ]</Link>
-                    </span> 
-                }
-              </div>
-            </div>
+  const forceUpdate = useForceUpdate();
+
+  const handleClick = () => {
+    forceUpdate();
+  };
+
+  return (
+    <div className="container mx-auto pt-8">  
+      <div className="pl-8 pr-12">
+        <div className="flex mb-4">
+          <div className="w-11/12">
+            <span className="center text-white text-xl">
+              <h1>Välkommen till nyhetsarkivet</h1>
+            </span>
+          </div>
+          <div className="w-1/12">
+            {
+              hasUserData ? 
+                <span className="center text-white test-sm">
+                  <Link to="/Logout" onClick={handleClick}>[ LOGGA UT ]</Link>
+                </span> 
+              : isLoginPage ? 
+                <span className="center text-white test-sm">
+                  <Link to="/" onClick={handleClick}>[ AVBRYT ]</Link>
+                </span> 
+              : 
+                <span className="center text-white test-sm">
+                  <Link to="/Login" onClick={handleClick}>[ LOGGA IN ]</Link>
+                </span> 
+            }
           </div>
         </div>
-
-  }
+      </div>
+    </div>
+  )
 }
 
 export default Header;
